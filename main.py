8f94d40906579c2f0903b5cbcdbb13f47add2b26
Ayush Rawat <ayushrawat324@gmail.com>
2022-09-12 23:42:54 +0530

    code asks input if any?

--- main.py
+++ main.py
@@ -8,35 +8,50 @@ headers = {
   'Content-Type': 'application/json'
 }
 
-choice = False
+run = False
 languages = ["cpp", "py", "java", "go","cs", "js"]
 option = 0
 code = ""
+input_ = ""
+input_run = False
 @app.route("/bot", methods=["POST"])
 def bot():
-    global choice, option,code 
+    global run, option,code,input_,input_run
     usr_msg = request.values.get("Body",'')
     bot_response = MessagingResponse()
     msg = bot_response.message()
-    if choice == True and usr_msg>='1' and usr_msg<='6':
-        option = int(usr_msg)-1
-        payload = json.dumps({
-            "code": code,
-            "language": languages[option],
-            "input": ""
-        })
-        response = requests.request("POST", url, headers=headers, data=payload)
-        if response.json()["success"] == False:
-            msg.body("Error! Please check the code again.")
-        elif response.json()["success"] == True:
-            msg.body(response.json()["output"])
-        choice =False
+    if run == True :
+        if usr_msg != "N/A" and input_run == True:
+            input_ = usr_msg
+            msg.body("Hello there! Still I am in developing stage.\nPress 1 to compile in c/c++ code\nPress 2 to compile in python code\nPress 3 to compile in java code\nPress 4 to compile in GoLang code\nPress 5 to compile in c# code\nPress 6 to compile in nodejs code")
+            input_run = False
+        elif usr_msg>='1' and usr_msg<='6':
+            option = int(usr_msg)-1
+            print(code)
+            payload = json.dumps({
+                "code": code,
+                "language": languages[option],
+                "input": input_
+            })
+            response = requests.request("POST", url, headers=headers, data=payload)
+            if response.json()["success"] == False:
+                msg.body("Error! Please check the code again.")
+            elif response.json()["success"] == True:
+                msg.body(response.json()["output"])
+            run =False
+        else:
+            msg.body("Wrong choice! Please type correct choice.")
 
-    elif choice == False:
+    elif run == False:
+        msg.body("Send your input if you have any. If no please type (N/A)")
         code = usr_msg
-        msg.body("Hello there! Still I am in developing stage so still I can run code without input only.\nPress 1 to compile in c/c++ code\nPress 2 to compile in python code\nPress 3 to compile in java code\nPress 4 to compile in GoLang code\nPress 5 to compile in c# code\nPress 6 to compile in nodejs code")
-        choice = True
-        
+        run = True
+        input_run = True
+        print(code)
+    
+    else:
+        msg.body("Please send code again.")
+        run = False
     
     return str(bot_response)
 
